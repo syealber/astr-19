c645773e83ac858af867e7f0c5ef5a1a403433da
syealber <102757933+syealber@users.noreply.github.com>
2022-04-22 16:49:04 -0700

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,22 @@
+print('Please input number')
+x = float(input())
+print('Please input second number to add')
+y = float(input())
+
+z = x + y
+z2 = int(y) - int(x)
+z3 = float(x) * int(y)
+
+print('sum')
+print(type(z))
+print(z)
+
+print('difference')
+print(type(z2))
+print(z2)
+
+print('product')
+print(type(z3))
+print(z3)
+
+
---
+++ prompt1j1.py
@@ -0,0 +1,6 @@
+x = 0
+while x == 0:
+    print("Full name: Sy Alberto")
+    print("My prefered pronouns are He/Him")
+    print("My favorite movie is Kung Fu Panda 2, because I like the emotional motivation behind the main character. My favorite food is 100% noodles, they go CRAZY.")
+    x += 1
